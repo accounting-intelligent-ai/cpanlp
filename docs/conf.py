82756c5a8cd4c69d616eb1f9aaf203f213a530f0
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-03-24 01:11:16 +0800

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Certified Public Accountant Nlp Project"
+copyright = "2023, Bfsu intelligent team"
+author = "Draco Deng"
 
 
 # -- General configuration ---------------------------------------------------
